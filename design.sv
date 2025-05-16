387baaf2b579a4415115833bc320223256bd2070
Shrinithi Srinivasan <154549418+Shrinithi-Srinivasan@users.noreply.github.com>
2025-05-17 00:27:14 +0530

    Create design.sv

---
+++ design.sv
@@ -0,0 +1,8 @@
+module d_flip_flop (input d,input clk,input rst,output reg q);
+  always @(posedge clk or negedge rst) begin
+    if (rst)
+      q <= 0; 
+    else
+      q <= d; 
+  end
+endmodule
